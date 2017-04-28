c248cdbbaa1b2773c52b7a9e539fd2b2d42790b9
Max Krieg <maxwell.krieg@gmail.com>
2017-04-28 16:44:39 -0400

    add rsvp processing, winner selection, better styling

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import bostonreactlogo from './boston-react-logo.jpg';
 import './App.css';
 import $ from 'jquery'
 
-const SIGNED_URL = 'https://api.meetup.com/ReactJS-Boston/events/238471436/rsvps?photo-host=public&sig_id=33956102&sig=87dd5c75fc1ca4b25a9bc1f781afe92c8e9408e8'
+
+const EVENT_ID = '238471436'
+const SIGNED_URL = `https://api.meetup.com/ReactJS-Boston/events/${EVENT_ID}/rsvps?photo-host=public&sig_id=33956102&sig=87dd5c75fc1ca4b25a9bc1f781afe92c8e9408e8`
 
 class App extends Component {
   state = {
-    requesting: false,
-    rsvps: null
+    rsvps: null,
+    winner: null
   }
 
   handleGetRsvps = () => {
     this.setState({ requesting: true })
     $.ajax({
-      dataType:'jsonp',
-      method:'get',
+      dataType: 'jsonp',
+      method: 'get',
       url: SIGNED_URL,
       success: (result) => {
-        console.log('back with ' + result.data.length +' results');
-        console.dir(result);
-        this.setState({ requesting: false, rsvps: result })
+        const rsvps = result.data.map((rsvp) => rsvp.member.name)
+        this.setState({ rsvps })
       }
     });
   }
 
+  handlePickWinner = () => {
+    const rsvpCount = this.state.rsvps.length
+    const winningIndex = Math.floor(Math.random() * rsvpCount) + 1
+    const winner = this.state.rsvps[winningIndex]
+    this.setState({ winner })
+  }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pluralsight ReactJS Meetup Raffle!!</h2>
+          <img src={bostonreactlogo} className="App-logo" alt="logo" />
+          <h2 style={{ fontSize: '32px' }}>Pluralsight ReactJS Meetup Raffle!!</h2>
         </div>
         <p className="App-intro">
           One lucky winner will get a year of Pluralsight for FREE!
         </p>
-        <button onClick={this.handleGetRsvps}>Get RSVP list</button>
-        <p>{this.state.requesting && 'Requesting rsvp list...'}</p>
-        <p>{this.state.rsvps ? 'Got rsvps!' : 'No rsvps yet :('}</p>
+        <p>{this.state.rsvps ? `Got ${this.state.rsvps.length} rsvps!` : 'No rsvps yet :('}</p>
+        <button disabled={this.state.rsvps} onClick={this.handleGetRsvps}>Get RSVP list</button>
+        <hr style={{ margin: '30px 20%' }}/>
+        <button disabled={!this.state.rsvps} onClick={this.handlePickWinner}>Pick a winner!</button>
+        <br/>
+        <p style={{ fontSize: '32px' }}>
+          {this.state.winner && this.state.winner.toUpperCase()}
+        </p>
       </div>
-    );
+    )
   }
 }
 

b9774b8a73dd2e6e86bf9695e0510c0277af4e85
Max Krieg <maxwell.krieg@gmail.com>
2017-05-23 12:25:59 -0400

    add input box for meetup link

--- src/App.js
+++ src/App.js
@@ -8,27 +8,38 @@ const URL = `https://api.meetup.com/ReactJS-Boston/events/239572732/rsvps?photo-
 class App extends Component {
   state = {
     rsvps: null,
-    winner: null
+    winner: null,
+    signedUrl: '',
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ signedUrl: e.target.value })
   }
 
   handleGetRsvps = () => {
-    this.setState({ requesting: true })
-    $.ajax({
-      dataType: 'jsonp',
-      method: 'get',
-      url: URL,
-      success: (result) => {
-        const rsvps = result.data.map((rsvp) => rsvp.member.name)
-        this.setState({ rsvps })
-      }
-    });
+    if (this.state.signedUrl) {
+      $.ajax({
+        dataType: 'jsonp',
+        method: 'get',
+        url: this.state.signedUrl,
+        success: (result) => {
+          const rsvps = result.data.map((rsvp) => rsvp.member.name)
+          this.setState({ rsvps })
+        }
+      });
+    }
   }
 
   handlePickWinner = () => {
     const rsvpCount = this.state.rsvps.length
     const winningIndex = Math.floor(Math.random() * rsvpCount) + 1
     const winner = this.state.rsvps[winningIndex]
-    this.setState({ winner })
+
+    if (!winner || this.state.winner === winner) {
+      this.handlePickWinner()
+    } else {
+      this.setState({ winner })
+    }
   }
 
   render() {
@@ -41,8 +52,16 @@ class App extends Component {
         <p className="App-intro">
           One lucky winner will get a year of Pluralsight for FREE!
         </p>
+        <label style={{ margin: '25px 0' }}>
+          Signed URL:<br/>
+          <input
+            style={{ width: "90%", height: "40px", fontSize: "20px" }}
+            value={this.state.signedUrl}
+            onChange={this.handleInputChange}
+          />
+        </label>
         <p>{this.state.rsvps ? `Got ${this.state.rsvps.length} rsvps!` : 'No rsvps yet :('}</p>
-        <button disabled={this.state.rsvps} onClick={this.handleGetRsvps}>Get RSVP list</button>
+        <button disabled={!this.state.signedUrl || this.state.rsvps} onClick={this.handleGetRsvps}>Get RSVP list</button>
         <hr style={{ margin: '30px 20%' }}/>
         <button disabled={!this.state.rsvps} onClick={this.handlePickWinner}>Pick a winner!</button>
         <br/>
